a0e9362a0418ab59b98567b236f74c493d9bce06
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-04-13 17:49:35 +0200

    ref

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 using namespace std;
 
+string padWithZero(int num);
+
 // TODO: check my questions. then probably brush up the knowledge on pointers and stuff
 int main() {
   // Get time
@@ -11,24 +13,24 @@ int main() {
   // Get appropriate values
   int year = nowStruct->tm_year + 1900;
   int month = nowStruct->tm_mon + 1;
-  string monthStr;
-  if (month < 10) monthStr = "0" + to_string(month);
-  else monthStr = to_string(month);
+  string monthStr = padWithZero(month);
   int day = nowStruct->tm_mday;
-  string dayStr;
-  if (day < 10) dayStr = "0" + to_string(day);
-  else dayStr = to_string(day);
+  string dayStr = padWithZero(day);
   int hours = nowStruct->tm_hour;
-  string hoursStr;
-  if (hours < 10) hoursStr = "0" + to_string(hours);
-  else hoursStr = to_string(hours);
+  string hoursStr = padWithZero(hours);
   int minutes = nowStruct->tm_min;
-  string minutesStr;
-  if (minutes < 10) minutesStr = "0" + to_string(minutes);
-  else minutesStr = to_string(minutes);
+  string minutesStr = padWithZero(minutes);
   // Glue appropriate values together
   string finalString = to_string(year) + "-" + monthStr + "-" + dayStr + " " + hoursStr + ":" + minutesStr;
 
   cout << finalString << endl;
   system("PAUSE");
+}
+
+string padWithZero(int num) {
+  string finalString;
+  if (num < 10) finalString = "0" + to_string(num);
+  else finalString = to_string(num);
+
+  return finalString;
 }
